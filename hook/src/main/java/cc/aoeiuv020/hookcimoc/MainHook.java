b814f62b490feaba874b3ca8d50bcbadbdf02f68
AoEiuV020 <aoeiuv020@gmail.com>
2022-12-12 22:16:03 +0800

    hook splash

--- hook/src/main/java/cc/aoeiuv020/hookcimoc/MainHook.java
+++ hook/src/main/java/cc/aoeiuv020/hookcimoc/MainHook.java
@@ -1,15 +1,10 @@
 package cc.aoeiuv020.hookcimoc;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
@@ -19,6 +14,26 @@ public class MainHook implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         XposedBridge.log("handleLoadPackage: " + lpparam.processName + ", " + lpparam.processName);
+        hookDebug(lpparam);
+        hookSplash(lpparam);
+
+    }
+
+    private void hookSplash(XC_LoadPackage.LoadPackageParam lpparam) {
+        XposedHelpers.findAndHookMethod(
+                "com.haleydu.cimoc.SplashActivity",
+                lpparam.classLoader, "initAd", XC_MethodReplacement.DO_NOTHING
+        );
+        XposedHelpers.findAndHookMethod(
+                "com.haleydu.cimoc.SplashActivity",
+                lpparam.classLoader, "showAD", XC_MethodReplacement.DO_NOTHING
+        );
+    }
+
+    private void hookDebug(XC_LoadPackage.LoadPackageParam lpparam) {
+        if (!BuildConfig.DEBUG) {
+            return;
+        }
         var r = new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
@@ -31,6 +46,5 @@ public class MainHook implements IXposedHookLoadPackage {
                 "android.app.Activity",
                 lpparam.classLoader, "onCreate", Bundle.class, r
         );
-
     }
 }
